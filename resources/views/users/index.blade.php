72a928e5592d12d599d9f70ef4289552661d4156
shigeoimafuku <pukurinshige@gmail.com>
2022-05-26 16:40:00 +0000

    user pages

---
+++ resources/views/users/index.blade.php
@@ -0,0 +1,6 @@
+@extends('layouts.app')
+
+@section('content')
+    {{-- ユーザー覧 --}}
+    @include('users.users')
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -23,3 +23,8 @@ Route::get('login','Auth\LoginController@showLoginForm')->name('login');
 Route::post('login','Auth\LoginController@login')->name('login.post');
 Route::get('logout','Auth\LoginController@logout')->name('logout.get');
 
+// 認証付きでユーザ情報へ
+Route::group(['middleware'=>['auth']],function(){
+    Route::resource('users','UsersController',['only'=>['index','show']]);
+});
+
